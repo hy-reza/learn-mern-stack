03828d170d878ad7808c5e1e1761130a98531293
hy-reza <93983098+hy-reza@users.noreply.github.com>
2022-03-26 12:36:36 +0700

    Setup DB connection

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import "dotenv/config";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
@@ -9,15 +10,17 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 app.use(cors());
+app.use("/posts", postRoutes); 
 
 const DB_URL = process.env.DB_URL;
 const PORT = 5000 || process.env.PORT;
 
 mongoose
-  .connect(DB_URL, {})
-  .then(
+  .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
     app.listen(PORT, () =>
       console.info(`Server is allready listening for port ${PORT}.... `)
     )
   )
-  .catch((e) => console.error(e));
+  .catch((error) => console.error(error));
+
