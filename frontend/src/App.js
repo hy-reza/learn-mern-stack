39966843ebeee80471e35e6af9c92c0e1573ceb3
hy-reza <93983098+hy-reza@users.noreply.github.com>
2022-03-27 13:17:01 +0700

    Earlly setup frontend layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Container, Grow, Grid, AppBar, Typography } from "@material-ui/core";
+import useStyles from "styles";
+
+import Post from "components/Posts/Posts";
+import Form from "components/Form/Form";
+
+import memories from "./images/memories.png";
 
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit" className={classes.appBar}>
+        <img
+          src={memories}
+          alt="memories"
+          height="60"
+          className={classes.image}
+        />
+        <Typography variant="h2" align="center" className={classes.heading}>
+          Memories
+        </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Post />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Posts/Posts.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Post from "./Post/Post";
+import useStyles from "styles";
+
+
+const Posts = () => {
+  const classes = useStyles() ;
+  return (
+    <div>
+      Post
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
